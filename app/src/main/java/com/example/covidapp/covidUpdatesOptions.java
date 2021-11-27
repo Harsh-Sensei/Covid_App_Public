3b23ec349e9a0d8e8cabf4877539455dde60a8e7
Harsh-Sensei <prodigy.harsh18@gmail.com>
2021-11-27 18:31:08 +0530

    Made UI attractive and added graphs

---
+++ app/src/main/java/com/example/covidapp/covidUpdatesOptions.java
@@ -0,0 +1,14 @@
+package com.example.covidapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class covidUpdatesOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covid_updates_options);
+    }
+}
